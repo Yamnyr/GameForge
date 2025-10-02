bfa629e0246c3e50c6619e9cd125994514826527
Corentin Bouet <corentinbouet77120@gmail.com>
2025-10-02 11:43:12 +0200

    Configuration du fichier admin.py

--- forgegame/admin.py
+++ forgegame/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Game, Character, Location, Favorite
 
-# Register your models here.
+# --------------------------
+# CustomUser
+# --------------------------
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('bio', 'avatar')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('bio', 'avatar')}),
+    )
+
+# --------------------------
+# Autres modèles
+# --------------------------
+class CharacterInline(admin.TabularInline):
+    model = Character
+    extra = 1
+
+class LocationInline(admin.TabularInline):
+    model = Location
+    extra = 1
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'user', 'is_public', 'created_at')
+    search_fields = ('title', 'genre', 'user__username')
+    list_filter = ('genre', 'is_public', 'created_at')
+    inlines = [CharacterInline, LocationInline]
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'game', 'created_at')
+    search_fields = ('user__username', 'game__title')
+
+# --------------------------
+# Enregistre les modèles
+# --------------------------
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(Character)
+admin.site.register(Location)
+admin.site.register(Favorite, FavoriteAdmin)
